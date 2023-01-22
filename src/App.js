e731acf0e577fcb16edf33195cd8cda3d6f4f7bb
shahaf <shahaf1131@gmail.com>
2023-01-22 21:45:04 +0200

    init proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { API, graphqlOperation } from 'aws-amplify'
+import { useEffect, useState } from 'react';
+
+import { ListTodos, UpdateTodo } from './graphql/queries';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    loadTodos()
+  }, [])
+
+  const loadTodos = async () => {
+    const todoData = await API.graphql(graphqlOperation(ListTodos))
+    console.log(todoData.data.listTodos.items);
+    setTodos(todoData.data.listTodos.items)
+  }
+
+  const toggleComplition = async (todoId) => {
+    const todo = todos.find(todo => todo.id === todoId)
+    todo.isCompleted = !todo.isCompleted
+    const updatedTodo = await API.graphql(graphqlOperation(UpdateTodo, { input: todo }))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map(todo => <p key={todo.id} onClick={() => toggleComplition(todo.id)}>{todo.title}</p>)}
     </div>
   );
 }
---
+++ src/AppSync.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+   "aws_appsync_graphqlEndpoint": "https://aysctqf3sjbljev2ma2qcllvga.appsync-api.eu-central-1.amazonaws.com/graphql",
+   "aws_appsync_region": "eu-central-1",
+   "aws_appsync_authenticationType": "API_KEY",
+   "aws_appsync_apiKey": "da2-y6s66574mrgl5luuuiy3qliwly",
+};
+
+export default awsmobile;
